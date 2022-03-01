c8c2e36cc1a381c3b5d93e938c5fdf8da5f3449d
Ardi <ardiasllani@hotmail.com>
2022-03-01 10:01:30 -0500

    pymongo connection and testing

--- backend/newsapi.py
+++ backend/newsapi.py
@@ -1,25 +1,57 @@
 from newsapi import NewsApiClient
 
-news_api_key = ''
+# importing pymongo
+import pymongo
+import urllib.parse
+from pymongo import MongoClient
 
 # Init
-newsapi = NewsApiClient(api_key=news_api_key)
+# newsapi = NewsApiClient(api_key='')
+# username = urllib.parse.quote_plus('')
+# password = urllib.parse.quote_plus('')
+
+
+# establing connection
+try:
+  cluster = MongoClient('mongodb://%s:%s@cluster0.go9eq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority' % (username, password))
+  print("Connected successfully!")
+except:
+  print("Could not connect to MongoDB")
+
+# connecting or switching to the database
+db = cluster["frontend"]
+collection = db["news"]
 
 # /v2/get_everything
 sources = newsapi.get_everything(q='crypto', page=1, page_size=5)
 posts = sources['articles']
 
 for post in posts:
-  print("Title:", post['title'].lower())
-  print("Description:", post['description'])
-  print("Image:", post['urlToImage'])
-  print("Published:", post['publishedAt'])
-  print("Source:", post['source']['name'])
-  print("Content:", post['content'])
-  print('URL: ', post['url'])
-  break
+  # first document
+  document = {
+    "title": post['title'],
+    "description": post['description'],
+    "image": post['urlToImage'],
+    "source": post['source']['name'],
+    "content": post['content'],
+    "url": post['url'],
+    "published": post['publishedAt']
+  }
+  
+  print(document)
+  # collection.insert_one(document)
+  # print("Title:", post['title'].lower())
+  # print("Description:", post['description'])
+  # print("Image:", post['urlToImage'])
+  # print("Published:", post['publishedAt'])
+  # print("Source:", post['source']['name'])
+  # print("Content:", post['content'])
+  # print('URL: ', post['url'])
+  # break
 
-# import posts in MongoDB
 
-# cron job every 5min?
-# check if post exist in DB, if URL equals URL do not import.
+# Printing the data inserted
+# cursor = collection.find()
+# print(cursor)
+# for record in cursor:
+#     print(record)
