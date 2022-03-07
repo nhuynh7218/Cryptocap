b0f868e4972caeff158d57ee511164e613b1e3a0
Ardi <ardiasllani@hotmail.com>
2022-03-07 17:12:25 -0500

    check if news already exists in the database

--- backend/newsapi.py
+++ backend/newsapi.py
@@ -4,22 +4,22 @@ import urllib.parse
 from pymongo import MongoClient
 from newsapi import NewsApiClient
 
-# Init newsapi lib
+# Init
 newsapi = NewsApiClient(api_key='03c5e649e30745138c3c93de33b8ad56')
 
 # Create a connection using MongoClient. You can import MongoClient or use pymongo.MongoClient
 client = MongoClient('mongodb+srv://cryptocap:abc123abc@cluster0.go9eq.mongodb.net/frontend?retryWrites=true&w=majority')
-
-# call mongodb function and select database name
+# call mongodb func
 db = client['frontend']
+# db = get_database()
 
 # select collection
 collection = db['news']
   
 # /v2/get_everything
-sources = newsapi.get_everything(q='crypto', page=1, page_size=5)
+sources = newsapi.get_everything(q='crypto', page=1, page_size=100)
 posts = sources['articles']
-x=0
+x=1
 for post in posts:
     # first document
     document = {
@@ -32,9 +32,14 @@ for post in posts:
       "published": post['publishedAt']
     }
     
-    collection.insert_one(document)
-    print("\n Imported ", x, "\n")
-    x+=1
+    exists = collection.count_documents({ "url": document['url'] }) > 0
+    if(exists == False):
+        collection.insert_one(document)
+        print("\n Imported ",x, " - ", document['url'], "\n")
+        x+=1
+    else:
+        print("\n News already imported. Skipped.")
+
     # print("Title:", post['title'].lower())
     # print("Description:", post['description'])
     # print("Image:", post['urlToImage'])
@@ -43,9 +48,3 @@ for post in posts:
     # print("Content:", post['content'])
     # print('URL: ', post['url'])
     # break
-
-# Printing the data inserted
-# cursor = collection.find()
-# print(cursor)
-# for record in cursor:
-#     print(record)
