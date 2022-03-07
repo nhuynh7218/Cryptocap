47c5a6a002232b03753211f41c57d1e5cd59ea5a
Ardi <ardiasllani@hotmail.com>
2022-03-07 11:20:07 -0500

    importing newsapi to mongodb

--- backend/newsapi.py
+++ backend/newsapi.py
@@ -1,40 +1,26 @@
-from django.db import models
-
 import newsapi
 import pymongo
 import urllib.parse
 from pymongo import MongoClient
 from newsapi import NewsApiClient
 
-# Init
+# Init newsapi lib
 newsapi = NewsApiClient(api_key='03c5e649e30745138c3c93de33b8ad56')
-username = urllib.parse.quote_plus('cryptocap')
-password = urllib.parse.quote_plus('abc123abc')
 
-def get_database():
-  # establing connection
-  try:
-    # Create a connection using MongoClient. You can import MongoClient or use pymongo.MongoClient
-    client = MongoClient('mongodb+srv://cryptocap:abc123abc@cluster0.go9eq.mongodb.net/frontend?retryWrites=true&w=majority')
-    print("Connected successfully!")
-  except:
-    print("Could not connect to MongoDB")
+# Create a connection using MongoClient. You can import MongoClient or use pymongo.MongoClient
+client = MongoClient('mongodb+srv://cryptocap:abc123abc@cluster0.go9eq.mongodb.net/frontend?retryWrites=true&w=majority')
 
-  db = client['frontend']
-  return db
+# call mongodb function and select database name
+db = client['frontend']
 
-if __name__ == "__main__":    
+# select collection
+collection = db['news']
   
-  # call mongodb func
-  db = get_database()
-  # select collection
-  collection = db['news']
-  
-  # /v2/get_everything
-  sources = newsapi.get_everything(q='crypto', page=1, page_size=5)
-  posts = sources['articles']
-
-  for post in posts:
+# /v2/get_everything
+sources = newsapi.get_everything(q='crypto', page=1, page_size=5)
+posts = sources['articles']
+x=0
+for post in posts:
     # first document
     document = {
       "title": post['title'],
@@ -46,22 +32,20 @@ if __name__ == "__main__":
       "published": post['publishedAt']
     }
     
-    print("\n\n---------\n\n")
-    
     collection.insert_one(document)
-    print("Title:", post['title'].lower())
-    print("Description:", post['description'])
-    print("Image:", post['urlToImage'])
-    print("Published:", post['publishedAt'])
-    print("Source:", post['source']['name'])
-    print("Content:", post['content'])
-    print('URL: ', post['url'])
-    break
+    print("\n Imported ", x, "\n")
+    x+=1
+    # print("Title:", post['title'].lower())
+    # print("Description:", post['description'])
+    # print("Image:", post['urlToImage'])
+    # print("Published:", post['publishedAt'])
+    # print("Source:", post['source']['name'])
+    # print("Content:", post['content'])
+    # print('URL: ', post['url'])
+    # break
 
-
-  # Printing the data inserted
-  # cursor = collection.find()
-  # print(cursor)
-  # for record in cursor:
-  #     print(record)
-
+# Printing the data inserted
+# cursor = collection.find()
+# print(cursor)
+# for record in cursor:
+#     print(record)
