4ef2007fcb27fcc1a518e7be2736b8e7f2bcd820
Ardi <ardiasllani@hotmail.com>
2022-03-07 11:15:30 -0500

    importing news to mongodb

--- backend/newsapi.py
+++ backend/newsapi.py
@@ -1,76 +1,67 @@
-from newsapi import NewsApiClient
+from django.db import models
 
-# importing pymongo
+import newsapi
 import pymongo
 import urllib.parse
 from pymongo import MongoClient
+from newsapi import NewsApiClient
 
 # Init
-# newsapi = NewsApiClient(api_key='')
-# username = urllib.parse.quote_plus('')
-# password = urllib.parse.quote_plus('')
+newsapi = NewsApiClient(api_key='03c5e649e30745138c3c93de33b8ad56')
+username = urllib.parse.quote_plus('cryptocap')
+password = urllib.parse.quote_plus('abc123abc')
 
+def get_database():
+  # establing connection
+  try:
+    # Create a connection using MongoClient. You can import MongoClient or use pymongo.MongoClient
+    client = MongoClient('mongodb+srv://cryptocap:abc123abc@cluster0.go9eq.mongodb.net/frontend?retryWrites=true&w=majority')
+    print("Connected successfully!")
+  except:
+    print("Could not connect to MongoDB")
 
-# establing connection
-try:
-  cluster = MongoClient('mongodb://%s:%s@cluster0.go9eq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority' % (username, password))
-  print("Connected successfully!")
-except:
-  print("Could not connect to MongoDB")
+  db = client['frontend']
+  return db
 
-# connecting or switching to the database
-db = cluster["frontend"]
-collection = db["news"]
-
-
-document = {
-    "title": 'TITLE',
-    "description": "description",
-    "image": "urlToImage"
-    # "source": post['source']['name'],
-    # "content": post['content'],
-    # "url": post['url'],
-    # "published": post['publishedAt']
-  }
-
-collection.insert_one(document)
-exit()
-
-# /v2/get_everything
-sources = newsapi.get_everything(q='crypto', page=1, page_size=5)
-posts = sources['articles']
-
-for post in posts:
-  # first document
-  document = {
-    "title": post['title'],
-    "description": post['description'],
-    "image": post['urlToImage'],
-    "source": post['source']['name'],
-    "content": post['content'],
-    "url": post['url'],
-    "published": post['publishedAt']
-  }
+if __name__ == "__main__":    
   
-  print(document)
-<<<<<<< Updated upstream
-=======
-  # print("\n\n---------\n\n")
->>>>>>> Stashed changes
-  # collection.insert_one(document)
-  break
-  # print("Title:", post['title'].lower())
-  # print("Description:", post['description'])
-  # print("Image:", post['urlToImage'])
-  # print("Published:", post['publishedAt'])
-  # print("Source:", post['source']['name'])
-  # print("Content:", post['content'])
-  # print('URL: ', post['url'])
-  # break
+  # call mongodb func
+  db = get_database()
+  # select collection
+  collection = db['news']
+  
+  # /v2/get_everything
+  sources = newsapi.get_everything(q='crypto', page=1, page_size=5)
+  posts = sources['articles']
+
+  for post in posts:
+    # first document
+    document = {
+      "title": post['title'],
+      "description": post['description'],
+      "image": post['urlToImage'],
+      "source": post['source']['name'],
+      "content": post['content'],
+      "url": post['url'],
+      "published": post['publishedAt']
+    }
+    
+    print("\n\n---------\n\n")
+    
+    collection.insert_one(document)
+    print("Title:", post['title'].lower())
+    print("Description:", post['description'])
+    print("Image:", post['urlToImage'])
+    print("Published:", post['publishedAt'])
+    print("Source:", post['source']['name'])
+    print("Content:", post['content'])
+    print('URL: ', post['url'])
+    break
 
 
-# Printing the data inserted
-# cursor = collection.find()
-# print(cursor)
-# for record in cursor:
-#     print(record)
+  # Printing the data inserted
+  # cursor = collection.find()
+  # print(cursor)
+  # for record in cursor:
+  #     print(record)
+
