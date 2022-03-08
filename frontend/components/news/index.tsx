d46c6208e94d57da6ddbdffa74b93fcf497e19f3
darren120 <zoudarren7@gmail.com>
2022-03-08 15:28:22 -0500

    more roubust page adding

--- frontend/components/news/index.tsx
+++ frontend/components/news/index.tsx
@@ -1,5 +1,26 @@
 import React from "react";
 import NewsLetter from "./news-letter";
+function CardCell(props: { newsArticle: any }) {
+    const { newsArticle } = props
+    return (
+        <div className="flex justify-center hover:bg-green-300 delay-50 duration-100 bg-gray-400 text-black p-5 rounded-lg w-60 group shadow-2xl ">
+
+            <Link href={`/`}>
+
+                <a className="" >
+                    <Image width={250} height={200} src={newsArticle.image} className=" shadow-2xl  rounded  hover:scale-105 duration-500 transition-all"></Image>
+                    <h1 className="font-bold text-lg  mt-5">{newsArticle.title}</h1>
+                    <h1 className="font-bold text-lg mt-5">{newsArticle.author}</h1>
+
+                    <h1 className="font-bold text-sm  ">{`${newsArticle.Description} `}</h1>
+                    <h1 className="font-bold text-sm  ">{`tags`}</h1>
+
+                </a>
+            </Link>
+
+        </div>
+    )
+}
 const BlogAuthor: React.FC<BlogAuthorProps> = (props) => {
     return (
         <HStack marginTop="2" spacing="2" display="flex" alignItems="center">
@@ -18,72 +39,72 @@ const BlogAuthor: React.FC<BlogAuthorProps> = (props) => {
 function BigNewsCell() {
 
     return (
-    <Box
-        marginTop={{ base: '1', sm: '5' }}
-        display="flex"
-        flexDirection={{ base: 'column', sm: 'row' }}
-        justifyContent="space-between">
         <Box
+            marginTop={{ base: '1', sm: '5' }}
             display="flex"
-            flex="1"
-            marginRight="3"
-            position="relative"
-            alignItems="center">
+            flexDirection={{ base: 'column', sm: 'row' }}
+            justifyContent="space-between">
             <Box
-                width={{ base: '100%', sm: '85%' }}
-                zIndex="2"
-                marginLeft={{ base: '0', sm: '5%' }}
-                marginTop="5%">
-                <Link textDecoration="none" _hover={{ textDecoration: 'none' }}>
-                    <Image
-                        borderRadius="lg"
-                        src={
-                            'https://images.unsplash.com/photo-1499951360447-b19be8fe80f5?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=800&q=80'
-                        }
-                        alt="some good alt text"
-                        objectFit="contain"
-                    />
-                </Link>
-            </Box>
-            <Box zIndex="1" width="100%" position="absolute" height="100%">
+                display="flex"
+                flex="1"
+                marginRight="3"
+                position="relative"
+                alignItems="center">
                 <Box
-                    bgGradient={useColorModeValue(
-                        'radial(orange.500 2px, transparent 2px)',
-                        'radial(orange.300 2px, transparent 4px)'
-                    )}
-                    backgroundSize="20px 20px"
-                    opacity="0.4"
-                    height="100%"
-                />
+                    width={{ base: '100%', sm: '85%' }}
+                    zIndex="2"
+                    marginLeft={{ base: '0', sm: '5%' }}
+                    marginTop="5%">
+                    <Link textDecoration="none" _hover={{ textDecoration: 'none' }}>
+                        <Image
+                            borderRadius="lg"
+                            src={
+                                'https://images.unsplash.com/photo-1499951360447-b19be8fe80f5?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=800&q=80'
+                            }
+                            alt="some good alt text"
+                            objectFit="contain"
+                        />
+                    </Link>
+                </Box>
+                <Box zIndex="1" width="100%" position="absolute" height="100%">
+                    <Box
+                        bgGradient={useColorModeValue(
+                            'radial(orange.500 2px, transparent 2px)',
+                            'radial(orange.300 2px, transparent 4px)'
+                        )}
+                        backgroundSize="20px 20px"
+                        opacity="0.4"
+                        height="100%"
+                    />
+                </Box>
             </Box>
-        </Box>
-        <Box
-            display="flex"
-            flex="1"
-            flexDirection="column"
-            justifyContent="center"
-            marginTop={{ base: '3', sm: '0' }}>
-            <BlogTags tags={['BTC', 'GLOBAL']} />
-            <Heading marginTop="1">
-                <Link textDecoration="none" _hover={{ textDecoration: 'none' }}>
-                    Blog article title
-                </Link>
-            </Heading>
-            <Text
-                as="p"
-                marginTop="2"
-                color={useColorModeValue('gray.700', 'gray.200')}
-                fontSize="lg">
-              {`  description description description description description description description description description description
+            <Box
+                display="flex"
+                flex="1"
+                flexDirection="column"
+                justifyContent="center"
+                marginTop={{ base: '3', sm: '0' }}>
+                <BlogTags tags={['BTC', 'GLOBAL']} />
+                <Heading marginTop="1">
+                    <Link textDecoration="none" _hover={{ textDecoration: 'none' }}>
+                        Blog article title
+                    </Link>
+                </Heading>
+                <Text
+                    as="p"
+                    marginTop="2"
+                    color={useColorModeValue('gray.700', 'gray.200')}
+                    fontSize="lg">
+                    {`  description description description description description description description description description description
                 description description description description description description description description description description
                 description description description description description description description description description description
                 description description description description description description description description description description
                 description description description description description description description description description description
                 description description description description description description description description description description...`}
-            </Text>
-            <BlogAuthor name="POTUS" date={new Date('2022-06-09T19:01:27Z')} />
-        </Box>
-    </Box>)
+                </Text>
+                <BlogAuthor name="POTUS" date={new Date('2022-06-09T19:01:27Z')} />
+            </Box>
+        </Box>)
 }
 const SmallNewsCell = () => {
     return (
@@ -115,7 +136,7 @@ const SmallNewsCell = () => {
                         </Link>
                     </Heading>
                     <Text as="p" fontSize="md" marginTop="2">
-                     {` Description Description Description Description Description 
+                        {` Description Description Description Description Description 
                      Description Description Description Description Description Description Description 
                      Description Description Description Description Description Description 
                      Description Description Description Description Description Description 
@@ -132,8 +153,30 @@ const SmallNewsCell = () => {
     );
 };
 
-
+interface NewsArticle {
+    author: string,
+    date: Date,
+    description: string,
+    image: string,
+    tags: string[]
+}
 function Index() {
+    const oneArticle: NewsArticle = {
+        author: 'Some Name',
+        date: new Date(),
+        description: 'akljsd ',
+        image: 'https://images.unsplash.com/photo-1499951360447-b19be8fe80f5?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=800&q=80',
+        tags: ['BTC', 'USA']
+
+
+    }
+    const newsList = () => {
+        var p = []
+        for (var i = 0; i < 20; i++) {
+            p.push(oneArticle)
+        }
+        return p
+    }
     return (
         <div className={`flex flex-col justify-center items-center`}>
 
@@ -157,9 +200,20 @@ function Index() {
             <div className={`flex flex-row`}>
                 <h1 className={'font-black text-xl'}>Discover</h1>
             </div>
-            <div className={'p-4'}>
-                <NewsLetter />
+
+           <div className="flex justify-center">
+           <div className={' grid px-2 sm:px-0 gap-4 grid-cols-2 md:grid-cols-3   lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 pt-4 pb-6'}>
+                {
+                    newsList().map((val, index) => {
+                        return (
+                            <CardCell newsArticle={val} key={index} />
+                        )
+                    })
+                }
             </div>
+           </div>
+
+
         </div>
     )
 }
