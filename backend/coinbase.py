d12d0716e069c4ea5570e0a1bab45871fbb31483
Ardi <ardiasllani@hotmail.com>
2022-03-08 18:21:59 -0500

    coinbase update

--- backend/coinbase.py
+++ backend/coinbase.py
@@ -1,19 +1,3 @@
-
-
-# https://cryptocap.digital/callback?code=027516fc91d91320a79582ab3d49c3700ed9027294c01065c779ac31b797e69d
-
-
-
-curl https://api.coinbase.com/oauth/token \
-  -X POST \
-  -d 
-'
-  grant_type=authorization_code
-  &code=027516fc91d91320a79582ab3d49c3700ed9027294c01065c779ac31b797e69d
-  &client_id=a412eac363577edbeff9d3b126a0168a83d5366fa2056893ec84a5eb8e3daf49
-  &client_secret=31eebc115cc456c40c24ae8f24186f868fbe815dcbdb29f72a034db82946b0a4
-'
-
 from .oauth import BaseOAuth2
 
 
