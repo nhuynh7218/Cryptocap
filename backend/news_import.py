438734f4615a46471674db795fc12c2de3b38525
Ardi <ardiasllani@hotmail.com>
2022-03-09 16:53:38 -0500

    Converted publishedAt to datetime

--- backend/news_import.py
+++ backend/news_import.py
@@ -1,14 +1,24 @@
-import newsapi
-import pymongo
-import urllib.parse
+import os, newsapi, pymongo, urllib.parse, datetime
+from dotenv import load_dotenv
 from pymongo import MongoClient
 from newsapi.newsapi_client import NewsApiClient
 
+# load dotenv lib
+load_dotenv()
+
+# NewsAPI API
+NewsAPI_KEY = os.getenv('NEWSAPI_KEY')
+
+# MongoDB config
+MongoDB_USER = os.getenv('MongoDB_USER')
+MongoDB_PASS = os.getenv('MongoDB_PASS')
+MongoDB_CLUSTER = os.getenv('MongoDB_CLUSTER')
+
 # Init
-newsapi = NewsApiClient(api_key='03c5e649e30745138c3c93de33b8ad56')
+newsapi = NewsApiClient(api_key=NewsAPI_KEY)
 
 # Create a connection using MongoClient. You can import MongoClient or use pymongo.MongoClient
-client = MongoClient('mongodb+srv://cryptocap:abc123abc@cluster0.go9eq.mongodb.net/frontend?retryWrites=true&w=majority')
+client = MongoClient('mongodb+srv://' + MongoDB_USER + ':' + MongoDB_PASS + '@' + MongoDB_CLUSTER + '/frontend?retryWrites=true&w=majority')
 
 # call mongodb func
 db = client['frontend']
@@ -22,6 +32,7 @@ posts = sources['articles']
 x=1
 for post in posts:
     # first document
+    published_date = datetime.datetime.strptime(post['publishedAt'], "%Y-%m-%dT%H:%M:%SZ")
     document = {
       "title": post['title'],
       "description": post['description'],
@@ -29,9 +40,9 @@ for post in posts:
       "source": post['source']['name'],
       "content": post['content'],
       "url": post['url'],
-      "published": post['publishedAt']
+      "published": published_date
     }
-    
+
     exists = collection.count_documents({ "url": document['url'] }) > 0
     if(exists == False):
         collection.insert_one(document)
