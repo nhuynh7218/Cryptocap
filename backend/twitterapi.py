b5840e9b6c2ffa4bcf031b627ad8b56dbe5f1d2c
Nick <nhuynh7218@bths.edu>
2022-03-14 23:06:06 -0400

    twitter api

--- backend/twitterapi.py
+++ backend/twitterapi.py
@@ -14,41 +14,30 @@ access_token_secret ='NtFxJEKNlZTCmCoMwhQSxiTRVaAghn0IamuRhhv1Xb6Wz'
 
 client = tweepy.Client(bearer_token=bearer_token, consumer_key = consumer_key, consumer_secret=consumer_secret, access_token=access_token, access_token_secret=access_token_secret)
 
-#queries = ['from:Cointelegraph', 'from:Crypto', 'from:CryptoBoomNews', 'from:BitcoinMagazine']
-#tweets = client.search_recent_tweets(query=query, media_fields = "preview_image_url",tweet_fields=['context_annotations', 'created_at'], max_results=10)
-userID = "Crypto"
-response = client.search_recent_tweets(("from:"+userID), tweet_fields = ['created_at','author_id','attachments'])
+#List of Twitter accounts to scrape from
+userID = ['Cointelegraph', 'Crypto', 'CryptoBoomNews', 'BitcoinMagazine']
+#iterates through each twitter account and get recent tweets, defaults to 10 tweets, can use max = xxx, up to 100
+for user in userID:
+    response = client.search_recent_tweets((("from:"+user+" -is:retweet")), tweet_fields = ['created_at','author_id','attachments'])
 # The method returns a Response object, a named tuple with data, includes,
 # errors, and meta fields
-print(response.meta)
+    print(response.meta)
 
 # In this case, the data field of the Response returned is a list of Tweet
 # objects
-tweets = response.data
-
-# Each Tweet object has default id and text fields
-for tweet in tweets:
-    document = {
-        "tweetID" : tweet.id, 
-        "text" : tweet.text,
-        "author ID": tweet.author_id,
-        "created at": tweet.created_at,
-        "URL": "https://twitter.com/"+userID+"/status/"+(str(tweet.id))
-    }
-    print(document)
-    print("\n\n---------\n\n")
-exit()
-for tweet in tweets:
-    print("tweetid: ", tweet.id)
-    print("text: ", tweet.text)
-    print("author ID: ", tweet.author_id)
-    print("attatchments: ", tweet.attachments)
-    print("created at: ", tweet.created_at)
-    print("URL:" "https://twitter.com/"+userID+"/status/"+(str(tweet.id)))
-    print("______________")
-
-
+    tweets = response.data
     
+# Each Tweet object has default id and text fields
+    for tweet in tweets:
+        document = {
+            "Username" : user,
+            "tweetID" : tweet.id, 
+            "text" : tweet.text,
+            "author ID": tweet.author_id,
+            "created at": tweet.created_at,
+            "URL": "https://twitter.com/"+user+"/status/"+(str(tweet.id))
+        }
+        print(document)
+        print("\n\n---------\n\n")
 
-exit()
 
