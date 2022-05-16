dc69b71eba223ee91620d2e3352e13b871be437b
Mike Altamirano <mikealta1@gmail.com>
2022-05-16 19:18:13 -0400

    changing disquslocation

--- frontend/components/news/article/index.tsx
+++ frontend/components/news/article/index.tsx
@@ -6,6 +6,7 @@ import { ArticleInfo } from "../../../interfaces/get";
 import { NewsCardCell } from "..";
 import { parseCookies, setCookie, destroyCookie } from 'nookies'
 import { addHours, differenceInHours, formatDistance } from "date-fns";
+import { DiscussionEmbed } from "disqus-react";
 
 export default function Article(props: {article: ArticleInfo}) {
   const { colorMode, toggleColorMode } = useColorMode();
@@ -76,99 +77,102 @@ export default function Article(props: {article: ArticleInfo}) {
        
       }
     return (
-        <div className="flex flex-col items-center space-y-4 py-4">
-           
-            <div className=" font-black text-center">
-                <motion.p animate={{x: ["-700%", "0%"]}} transition={{duration: 0.75}}>{d.toDateString()}</motion.p>
-                <motion.p animate={{x: ["700%", "0%"]}} transition={{duration: 0.75}}> 4 Mins Ago</motion.p>
-            </div>
-            <motion.p animate={{y: ["-700%", "80%","-30%", "0%"]}} transition={{duration: 1, delay:0.75}} className=" font-extrabold text-2xl underline">{article.title}</motion.p>
-<motion.div animate={{scale:[0,1]}} transition={{duration: 0.55, delay:0.75}}>
-<Image width={350} height={300} src={article.image} className=" shadow-2xl rounded  duration-500 transition-all"></Image>
+        <><div className="flex flex-col items-center space-y-4 py-4">
 
-</motion.div>
-           <div className="flex flex-col">
-            {/* <Link className=" font-bold text-md "> <SplitText 
-                initial={{ y: '100%' }}
-                animate="visible"
-                variants={{
-                  visible: (i: number) => ({
-                    y: 0,
-                    transition: {
-                      delay: i * 0.33
-                    }
-                  })
-                }}
-              >
-              {"By: " + article.author}
-              </SplitText></Link> */}
-              <Link href={`${article.url}`}  rel="noreferrer" target="_blank" className=" font-bold text-md "> <SplitText 
-                initial={{ y: '100%' }}
-                animate="visible"
-                variants={{
-                  visible: (i: number) => ({
-                    y: 0,
-                    transition: {
-                      delay: i * 0.33
-                    }
-                  })
-                }}
-              >
-             {"Source: " + article.source}
-              </SplitText></Link>
-           </div>
+        <div className=" font-black text-center">
+          <motion.p animate={{ x: ["-700%", "0%"] }} transition={{ duration: 0.75 }}>{d.toDateString()}</motion.p>
+          <motion.p animate={{ x: ["700%", "0%"] }} transition={{ duration: 0.75 }}> 4 Mins Ago</motion.p>
+        </div>
+        <motion.p animate={{ y: ["-700%", "80%", "-30%", "0%"] }} transition={{ duration: 1, delay: 0.75 }} className=" font-extrabold text-2xl underline">{article.title}</motion.p>
+        <motion.div animate={{ scale: [0, 1] }} transition={{ duration: 0.55, delay: 0.75 }}>
+          <Image width={350} height={300} src={article.image} className=" shadow-2xl rounded  duration-500 transition-all"></Image>
 
-
-            <div className="flex flex-row justify-between space-x-4">
-                <button onClick={async () => await vote(true)}><svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 hover:text-green-400" fill="green" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M14 10h4.764a2 2 0 011.789 2.894l-3.5 7A2 2 0 0115.263 21h-4.017c-.163 0-.326-.02-.485-.06L7 20m7-10V5a2 2 0 00-2-2h-.095c-.5 0-.905.405-.905.905 0 .714-.211 1.412-.608 2.006L7 11v9m7-10h-2M7 20H5a2 2 0 01-2-2v-6a2 2 0 012-2h2.5" />
-                </svg></button>
-                <p className="font-black text-md cursor-default transition-all">{totalVotes}</p>
-                <button onClick={async () => await vote(false)}><svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 hover:text-pink-400" fill="red" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M10 14H5.236a2 2 0 01-1.789-2.894l3.5-7A2 2 0 018.736 3h4.018a2 2 0 01.485.06l3.76.94m-7 10v5a2 2 0 002 2h.096c.5 0 .905-.405.905-.904 0-.715.211-1.413.608-2.008L17 13V4m-7 10h2m5-10h2a2 2 0 012 2v6a2 2 0 01-2 2h-2.5" />
-                </svg></button>
-
-            </div>
-            <div className="font-semibold   px-10 md:px-20 lg:px-0 lg:w-3/4 xl:w-3/5">
-           
-            </div>
-     
-            <motion.p initial={container.hidden}  animate={container.visible} variants={container} className="font-semibold  text-center  px-10 md:px-20 lg:px-0 lg:w-3/4 xl:w-3/5 ">
-                {article.content}
-            </motion.p>
-            <Link  href={props.article.url} rel="noreferrer" target="_blank" className={` px-6 text-center text-bold  py-2 ${colorMode == "light" ? 'bg-green-400 hover:bg-green-300 ' : 'bg-green-700 hover:bg-green-600' } rounded-md`}>Read More</Link>
-
-            <Divider  className="py-6 mx-5"/>
-
-            <div className="flex flex-col justify-center">
-                {relatedArticle ? 
-                <div>
-                    <h1 className="w-full font-black text-lg">Related</h1>
-                <div className={' grid px-2 sm:px-0 gap-4 grid-cols-2 md:grid-cols-3   lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 pt-4 pb-6'}>
-                    {
-                        relatedArticle.map((val, index) => {
-                            return (
-                                <NewsCardCell newsArticle={val} key={index} />
-                            )
-                        })
-                    }
-                </div> 
-                </div>
-                :
-                <div>
-                    
-                    <Spinner
-                                thickness='4px'
-                                speed='0.65s'
-                                emptyColor='gray.200'
-                                color='blue.500'
-                                size='xl'
-                            />
-                </div>
+        </motion.div>
+        <div className="flex flex-col">
+          {/* <Link className=" font-bold text-md "> <SplitText
+        initial={{ y: '100%' }}
+        animate="visible"
+        variants={{
+          visible: (i: number) => ({
+            y: 0,
+            transition: {
+              delay: i * 0.33
+            }
+          })
+        }}
+      >
+      {"By: " + article.author}
+      </SplitText></Link> */}
+          <Link href={`${article.url}`} rel="noreferrer" target="_blank" className=" font-bold text-md "> <SplitText
+            initial={{ y: '100%' }}
+            animate="visible"
+            variants={{
+              visible: (i: number) => ({
+                y: 0,
+                transition: {
+                  delay: i * 0.33
                 }
-            </div>
+              })
+            }}
+          >
+            {"Source: " + article.source}
+          </SplitText></Link>
+        </div>
+
+
+        <div className="flex flex-row justify-between space-x-4">
+          <button onClick={async () => await vote(true)}><svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 hover:text-green-400" fill="green" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M14 10h4.764a2 2 0 011.789 2.894l-3.5 7A2 2 0 0115.263 21h-4.017c-.163 0-.326-.02-.485-.06L7 20m7-10V5a2 2 0 00-2-2h-.095c-.5 0-.905.405-.905.905 0 .714-.211 1.412-.608 2.006L7 11v9m7-10h-2M7 20H5a2 2 0 01-2-2v-6a2 2 0 012-2h2.5" />
+          </svg></button>
+          <p className="font-black text-md cursor-default transition-all">{totalVotes}</p>
+          <button onClick={async () => await vote(false)}><svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 hover:text-pink-400" fill="red" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M10 14H5.236a2 2 0 01-1.789-2.894l3.5-7A2 2 0 018.736 3h4.018a2 2 0 01.485.06l3.76.94m-7 10v5a2 2 0 002 2h.096c.5 0 .905-.405.905-.904 0-.715.211-1.413.608-2.008L17 13V4m-7 10h2m5-10h2a2 2 0 012 2v6a2 2 0 01-2 2h-2.5" />
+          </svg></button>
 
         </div>
+        <div className="font-semibold   px-10 md:px-20 lg:px-0 lg:w-3/4 xl:w-3/5">
+
+        </div>
+
+        <motion.p initial={container.hidden} animate={container.visible} variants={container} className="font-semibold  text-center  px-10 md:px-20 lg:px-0 lg:w-3/4 xl:w-3/5 ">
+          {article.content}
+        </motion.p>
+        <Link href={props.article.url} rel="noreferrer" target="_blank" className={` px-6 text-center text-bold  py-2 ${colorMode == "light" ? 'bg-green-400 hover:bg-green-300 ' : 'bg-green-700 hover:bg-green-600'} rounded-md`}>Read More</Link>
+
+        <Divider className="py-6 mx-5" />
+
+        <div className="flex flex-col justify-center">
+          {relatedArticle ?
+            <div>
+              <h1 className="w-full font-black text-lg">Related</h1>
+              <div className={' grid px-2 sm:px-0 gap-4 grid-cols-2 md:grid-cols-3   lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 pt-4 pb-6'}>
+                {relatedArticle.map((val, index) => {
+                  return (
+                    <NewsCardCell newsArticle={val} key={index} />
+                  );
+                })}
+              </div>
+            </div>
+            :
+            <div>
+
+              <Spinner
+                thickness='4px'
+                speed='0.65s'
+                emptyColor='gray.200'
+                color='blue.500'
+                size='xl' />
+            </div>}
+        </div>
+
+      </div><div className="flex flex-col mx-48"><DiscussionEmbed
+        shortname='Cryptocap'
+        config={{
+          url: 'https://cryptocap.digital/news/article/' + props.article._id,
+          identifier: 'cryptocap' + props.article._id,
+          title: 'cryptocap' + props.article._id,
+          language: 'eng'
+        }} /></div></>
     )
 }
 //@ts-ignore
